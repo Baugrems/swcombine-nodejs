8cbf6bebf3d153b544a3146ff300c45c44e8eebb
noctisdb <d.benn87@gmail.com>
2018-11-03 22:21:10 -0500

    GET

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes for site. Likely refactor later into separate files.
+//hardcode username for now
 app.get("/", function(req,res){
-	res.render("home")
+	res.render("home");
 })
-
+app.get("/market", function(req,res){
+	res.render("market", {title: "Noctis"});
+})
+app.get("*", function(req,res){
+	res.render("error");
+})
 
 // listner for server start
 app.listen("8080", "127.0.0.1", function(){
