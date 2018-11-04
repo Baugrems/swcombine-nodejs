fe5151a8299de49f847476a63d1c28f846efc84f
Baugrems <baugrems@swcombine.com>
2018-11-04 03:55:58 -0700

    add mysql and test functions

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+var con = mysql.createConnection({
+  host: "localhost",
+  database: "swc",
+  user: "root",
+  password: "localhost",
+  insecureAuth : true
+});
+
+con.connect(function(err) {
+ 	if (err) throw err;
+  	console.log("Connected!");
+});
+
+
+// TEST QUERY FOR DATABASE
+// con.query('SELECT * FROM factionTypes', function (error, results, fields) {
+//     if (error)
+//         throw error;
+
+//     results.forEach(result => {
+//         console.log(result);
+//     });
+// });
+
 // Routes for site. Likely refactor later into separate files.
 app.get("/", function(req,res){
-	res.render("home")
-})
+	con.query('SELECT * FROM characters', function (error, results, fields) {
+	if (error)
+		throw error;
 
+	res.render("home", {results: results});
+	});
+});
 
 // listner for server start
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started");
 });
+
