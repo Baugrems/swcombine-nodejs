e1d116a7a963636a0343382ba579911b316c3674
Baugrems <baugrems@swcombine.com>
2018-11-07 19:13:43 -0700

    revert away from Pug

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+<<<<<<< HEAD
 const pug = require('pug');
 
 var shipClass = 0;
+=======
+>>>>>>> parent of 833fab5... Add selection for ships based on type
 
 app.use(express.static(__dirname + '/public'));
 app.set("view engine", "ejs");
@@ -26,31 +29,29 @@ con.connect(function(err) {
 
 
 // TEST QUERY FOR DATABASE
+<<<<<<< HEAD
 con.query('SELECT * FROM shipTypes', function (error, results, fields) {
     if (error)
         throw error;
+=======
+// con.query('SELECT * FROM factionTypes', function (error, results, fields) {
+//     if (error)
+//         throw error;
+>>>>>>> parent of 833fab5... Add selection for ships based on type
 
-    results.forEach(result => {
-        console.log(result);
-    });
-});
+//     results.forEach(result => {
+//         console.log(result);
+//     });
+// });
 
 // Routes for site. Likely refactor later into separate files.
 app.get("/", function(req,res){
-	con.query('SELECT * FROM shipTypes ORDER BY name ASC', function (error, results, fields) {
+	con.query('SELECT * FROM characters', function (error, results, fields) {
 	if (error)
 		throw error;
-	res.render("home", {results: results, shipClass: shipClass});
-	});
-});
 
-app.post("/", function(req,res){
-  shipClass = req.body.shipclass
-  con.query('SELECT * FROM shipTypes ORDER BY name ASC', function (error, results, fields) {
-  if (error)
-    throw error;
-  res.render("home", {results: results, shipClass: shipClass});
-  });
+	res.render("home", {results: results});
+	});
 });
 
 // listner for server start
